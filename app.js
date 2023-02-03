956e72116fdc12d85562f13e18c753a6cc0b44f2
Anupam Singh <anupamwelkin@gmail.com>
2023-02-03 11:01:14 +0530

    added edit option and all pages

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ let modalDiv = document.getElementById("modalDiv");
 
 let notesArr = [];
 
-if (sessionStorage.getItem("Title") === null) {
-  let str = "";
-  str += `<p style="text-align:center; font-size:23px" class="my-3">Nothing to show</p>`;
-  noteCard.innerHTML = str;
-}
-
 function validate() {
   if (noteTitle.value == "") {
     alert("Title can't be null");
@@ -28,58 +22,97 @@ function submitFn() {
 function saveLocalData() {
   let strArr = { Title: noteTitle.value, Description: noteDesc.value };
   notesArr.push(strArr);
-  sessionStorage.setItem("notes", JSON.stringify(notesArr));
+  localStorage.setItem("notes", JSON.stringify(notesArr));
   populateData();
+  noteTitle.value = "";
+  noteDesc.value = "";
 }
 function populateData() {
-  let html = "";
+  if (
+    JSON.parse(localStorage.getItem("notes")) === null ||
+    JSON.parse(localStorage.getItem("notes")).length === 0
+  ) {
+    let str = "";
+    str += `<p style="text-align:center; font-size:23px" class="my-3">Add Some Notes</p>`;
+    noteCard.innerHTML = str;
+  } else {
+    let html = "";
 
-  let data = JSON.parse(sessionStorage.getItem("notes"));
-  data.forEach((item, index) => {
-    html += ` 
+    let data = JSON.parse(localStorage.getItem("notes"));
+    data.forEach((item, index) => {
+      html += ` 
   <div class="card mx-3 my-3" style="width: 18rem" >
    <div class="card-body">
-                <h3>${index + 1}</h3>
+                <h3 style="display:none">${index + 1}</h3>
                 <h5 class="card-title">${item.Title}</h5>
                 <p class="card-text">${item.Description}</p>
                 <div class="btnContainer d-flex flex-row justify-content-between">
                  
-                <button href="#" class="btn btn-primary" id="myBtn" onclick="editFn(this)" >Edit</button>
+                <button href="#" id=${index} class="btn btn-primary" id="myBtn" onclick="editFn(this)" >Edit</button>
                     <button href="#" id=${index} class="btn btn-danger" onclick="deleteFn(this.id)">Delete</button>
                  </div>
              </div>
              </div>`;
-  });
-  noteCard.innerHTML = html;
+    });
+    noteCard.innerHTML = html;
+  }
 }
 
-function editFn() {
-  console.log("running modal");
-  // let modalStr = "";
-  // let data = JSON.parse(sessionStorage.getItem("notes"));
-  // data.map((item) => {
-  //   modalStr += ` //   <div class="modal-content">
-  //     <span class="close">&times;</span>
-  //     <p>Title: </p>
-  //     <input type = "text" class="form-control" value=${this.value}>
-  //   </div>
+function editFn(e) {
+  openModal();
 
-  // `;
-  // });
-  // modalDiv.innerHTML = modalStr;
-  // alert();
+  let editSno =
+    e.parentElement.previousElementSibling.previousElementSibling
+      .previousElementSibling.innerHTML;
+
+  let editTitle =
+    e.parentElement.previousElementSibling.previousElementSibling.innerHTML;
+  let editDescription = e.parentElement.previousElementSibling.innerHTML;
+  // console.log(editTitle, editDescription);
+  let modalStr = "";
+  let data = JSON.parse(localStorage.getItem("notes"));
+  modalStr += `
+   <div class="modal-content">
+      <span class="close" onclick="closeModal()">&times;</span>
+      <label><b> Title : </b>
+      <input type = "text" id="edInpTitle" class="form-control" /></label><br>
+      <label> <b> Description : </b>
+      <input type = "text" id="edInpTextarea" class="form-control" /></label> <br>
+      <button id=${
+        editSno - 1
+      } onclick="insertUpdate(this.id);"  class="btn btn-primary" >Update</button>
+    </div>
+  `;
+  modalDiv.innerHTML = modalStr;
+  document.getElementById("edInpTitle").value = editTitle;
+  document.getElementById("edInpTextarea").value = editDescription;
+}
+function insertUpdate(e) {
+  let dataTitle = document.getElementById("edInpTitle").value;
+  let dataDesc = document.getElementById("edInpTextarea").value;
+  let updateData = { Title: dataTitle, Description: dataDesc };
+  let editData = localStorage.getItem("notes");
+  let parseEditData = JSON.parse(editData);
+  parseEditData.splice(e, 1);
+  parseEditData.push(updateData);
+  localStorage.setItem("notes", JSON.stringify(parseEditData));
+  populateData();
+  closeModal();
 }
 function deleteFn(index) {
-  // console.log("calling delete");
-
-  let notesArr = sessionStorage.getItem("notes");
+  let notesArr = localStorage.getItem("notes");
   let data = JSON.parse(notesArr);
-  // console.log(data);
-
   data.splice(index, 1);
-  sessionStorage.setItem("notes", JSON.stringify(data));
+  localStorage.setItem("notes", JSON.stringify(data));
   populateData();
 }
 window.addEventListener("DOMContentLoaded", () => {
   populateData();
 });
+
+function closeModal() {
+  modalDiv.style.display = "none";
+}
+function openModal() {
+  modalDiv.style.display = "block";
+}
